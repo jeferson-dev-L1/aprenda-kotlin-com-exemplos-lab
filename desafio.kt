09a3aa0db34aa71d47b11a4a4c233a0adf5b243e
jeferson-dev-L1 <jefinho_m_@outlook.com>
2025-11-15 16:00:06 -0400

    Alteração compelta

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,74 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+num class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario (var nome: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(
+    var nome: String, 
+    val duracao: Int = 60,
+    val nivel: Nivel = Nivel.BASICO 
+)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+data class Formacao(
+    var nome: String,
+    val nivel: Nivel,
+    var conteudos: List<ConteudoEducacional>) {
+    
     val inscritos = mutableListOf<Usuario>()
     
+    val duracaoTotal: Int
+        get()= conteudos.sumOf{it.duracao}  
+    
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if(inscritos.contains(usuario)){
+            println("X ${usuario.nome} já está matriculado(a) na formação $nome.")
+            return
+        }
+        
+        inscritos.add(usuario)
+        println("${usuario.nome} matriculado(a) com sucesso na formação $nome!")
+    }
+    
+    fun matricularVarios(vararg novosUsuarios: Usuario){
+        novosUsuarios.forEach{ usuario -> 
+            matricular(usuario)
+        }
+    }
+}
+            
+fun main() {
+    val kotlinIntro = ConteudoEducacional("Introdução ao Kotlin", 60, Nivel.BASICO)
+    val kotlinOO = ConteudoEducacional("Orientação a objetos com Kotlin", 100, Nivel.INTERMEDIARIO)
+    val coroutines = ConteudoEducacional("Coroutines Avançadas", 250, Nivel.AVANCADO)
+    
+    val formacaoBackend = Formacao(
+        nome = "Kotlin Back-end Developer",
+        nivel = Nivel.AVANCADO,
+        conteudos = listOf(kotlinIntro, kotlinOO, coroutines)
+    )
+    
+    val aluno1 = Usuario("Jeferson", "jeferson@dio.me")
+    val aluno2 = Usuario("Marcos", "marcos@dio.me")
+    val aluno3 = Usuario("Daniela", "daniela@dio.me")
+    
+    println("---Teste de matricula em: ${formacaoBackend.nome}---")
+    
+    formacaoBackend.matricular(aluno1)
+    formacaoBackend.matricular(aluno2)
+    formacaoBackend.matricular(aluno1) // Aluno já matriculado
+    
+    val outrosAlunos = listOf(aluno3, Usuario("Raquel", "raquel@dio.me")).toTypedArray()
+    formacaoBackend.matricularVarios(*outrosAlunos)
+    
+    println("\n--- Resumo da Formação ---")
+    println("Formação: ${formacaoBackend.nome} (${formacaoBackend.nivel})")
+    println("Duração Estimada: ${formacaoBackend.duracaoTotal} minutos")
+    println("Total de Módulos: ${formacaoBackend.conteudos.size}") 
+    
+    println("\n**Alunos Matriculados (${formacaoBackend.inscritos.size}):**")
+    formacaoBackend.inscritos.forEachIndexed { index, aluno ->
+        println("${index + 1}. ${aluno.nome} - Email: ${aluno.email}")
     }
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
